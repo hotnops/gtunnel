9ff304da82741ef6787f936b496567b370996de2
hotnops <hotnops@protonmail.com>
2020-04-28 06:45:55 -0700

    Using --net host instead of -p so that any port used will be accessible by user.

--- start_server.sh
+++ start_server.sh
@@ -1,2 +1,2 @@
 docker build -f gServer/Dockerfile . -t gtunnel-server
-docker run -it --rm -p 443:443 -v $PWD/configured:/go/src/gTunnel/configured --name gtun-server gtunnel-server
+docker run -it --rm --net host -v $PWD/configured:/go/src/gTunnel/configured --name gtun-server gtunnel-server
