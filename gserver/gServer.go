1e077690d5029f1d7e2b52bf1d7eefe128065bfa
hotnops <hotnops@protonmail.com>
2020-12-31 11:32:44 -0800

    Redirecting all log messages to log file

--- gserver/gServer.go
+++ gserver/gServer.go
@@ -2,6 +2,11 @@ package main
 
 import (
 	"flag"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+	"time"
 
 	"github.com/hotnops/gTunnel/gserver/gserverlib"
 )
@@ -12,13 +17,31 @@ var (
 	keyFile    = flag.String("key_file", "tls/key", "The TLS key file")
 	clientPort = flag.Int("clientPort", 443, "The server port")
 	adminPort  = flag.Int("adminPort", 1337, "The server port")
+	logfile    = flag.String("logFile", "", "The file where log output will be written")
 )
 
 // What it do
 func main() {
 	flag.Parse()
 
+	var filePath = ""
 	s := gserverlib.NewGServer()
+
+	if *logfile == "" {
+		time := strings.ReplaceAll(time.Now().UTC().String(), " ", "")
+		filePath = fmt.Sprintf("logs/gtunnel_%s.log", time)
+	} else {
+		filePath = *logfile
+	}
+
+	file, err := os.OpenFile(filePath, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0666)
+
+	if err != nil {
+		log.Fatalf("[!] Failed to create log file.")
+	}
+
+	log.SetOutput(file)
+
 	s.Start(*clientPort, *adminPort, *tls, *certFile, *keyFile)
 
 }
