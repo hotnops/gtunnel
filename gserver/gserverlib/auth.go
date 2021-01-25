c6fd7f16d4d6b80b2d2ae88fed2925c3214b61ab
hotnops <hotnops@protonmail.com>
2021-01-25 08:25:41 -0800

    Adding missing files

---
+++ gserver/gserverlib/auth.go
@@ -0,0 +1,65 @@
+package gserverlib
+
+import (
+	"context"
+	"crypto/rand"
+	"log"
+	"math/big"
+	"strings"
+
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/metadata"
+	"google.golang.org/grpc/status"
+)
+
+// GenerateToken will generate a random string to be
+// used as a client authorization.
+func GenerateToken() (string, error) {
+	// Minimum token size is 24, max is 36
+	reader := rand.Reader
+	max := big.NewInt(int64(MaxTokenSize - MinTokenSize))
+	min := big.NewInt(int64(MinTokenSize))
+
+	tokenSize, err := rand.Int(reader, max)
+
+	if err != nil {
+		log.Print("[!] Failed to generate a password size\n")
+	}
+
+	tokenSize.Add(tokenSize, min)
+
+	var letters = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()=_+[]{};,.<>/?")
+
+	b := make([]rune, tokenSize.Int64())
+	for i := range b {
+		newRand, _ := rand.Int(reader, big.NewInt(int64(len(letters))))
+		b[i] = letters[newRand.Int64()]
+	}
+
+	return string(b), nil
+}
+
+// GetClientInfoFromCtx will return the bearer token string
+// from the auth header within the ctx structure.
+func GetClientInfoFromCtx(ctx context.Context) (string, string, error) {
+	md, ok := metadata.FromIncomingContext(ctx)
+
+	if !ok {
+		return "", "", status.Error(codes.InvalidArgument, "Failed to retrieve metadata")
+	}
+
+	authHeader, ok := md["authorization"]
+	if !ok {
+		return "", "", status.Errorf(codes.Unauthenticated, "Authorization token is not provided")
+	}
+
+	bearerToken := authHeader[0]
+	if !strings.HasPrefix(bearerToken, bearerString) {
+		return "", "", status.Errorf(codes.InvalidArgument, "Invalid authorization header")
+	}
+
+	auth := strings.Split(bearerToken, bearerString)[1]
+	token := strings.Split(auth, "-")[0]
+	uuid := strings.Split(auth, "-")[1]
+	return token, uuid, nil
+}
---
+++ gserver/gserverlib/configStore.go
@@ -0,0 +1,113 @@
+package gserverlib
+
+import (
+	"context"
+	"encoding/json"
+	"log"
+	"sync"
+
+	"github.com/go-redis/redis/v8"
+)
+
+// ConfigStore is a structure that represents all of the configurations of
+// the gServer and will keep state to a json file
+type ConfigStore struct {
+	// This map keeps all of the configured clients in a store that
+	// uses their bearer token as a key for easy auth lookup
+	configuredClients map[string]*ConfiguredClient
+
+	// The filename where the configuration will save changes and load
+	// on start
+	redisClient *redis.Client
+	context     context.Context
+	mutex       sync.Mutex
+}
+
+func NewConfigStore() *ConfigStore {
+	configStore := new(ConfigStore)
+	configStore.context = context.Background()
+	configStore.redisClient = redis.NewClient(&redis.Options{
+		Addr:     "localhost:6379",
+		Password: "",
+		DB:       0,
+	})
+
+	configStore.configuredClients = make(map[string]*ConfiguredClient)
+
+	return configStore
+}
+
+func (c *ConfigStore) AddConfiguredClient(key string, client *ConfiguredClient) error {
+
+	clientJSON, err := json.Marshal(client)
+
+	if err != nil {
+		log.Printf("[!] Failed to convert configured client into json")
+		return err
+	}
+
+	// Make sure that our operations are atmoic
+	c.mutex.Lock()
+	defer c.mutex.Unlock()
+
+	err = c.redisClient.Set(c.context, key, clientJSON, 0).Err()
+	if err != nil {
+		log.Printf("[!] Failed to insert configured client into redis database")
+		return err
+	}
+
+	c.configuredClients[key] = client
+
+	return nil
+}
+
+func (c *ConfigStore) GetConfiguredClient(key string) *ConfiguredClient {
+	client, ok := c.configuredClients[key]
+	if !ok {
+		return nil
+	}
+	return client
+}
+
+func (c *ConfigStore) DeleteConfiguredClient(key string) error {
+	c.mutex.Lock()
+	defer c.mutex.Unlock()
+
+	err := c.redisClient.Del(c.context, key).Err()
+	if err != nil {
+		log.Printf("[!] Failed to delete configured client")
+		return err
+	}
+
+	delete(c.configuredClients, key)
+
+	return nil
+}
+
+func (c *ConfigStore) Initialize() error {
+	c.mutex.Lock()
+	defer c.mutex.Unlock()
+
+	keys, err := c.redisClient.Keys(c.context, "*").Result()
+
+	if err != nil {
+		log.Printf("[!] Failed to initialize configuration store")
+		return err
+	}
+
+	for _, key := range keys {
+		clientConfig := new(ConfiguredClient)
+
+		value, err := c.redisClient.Get(c.context, key).Result()
+
+		err = json.Unmarshal([]byte(value), clientConfig)
+		if err != nil {
+			log.Printf("[!] Failed to load configured client")
+			continue
+		}
+
+		c.configuredClients[key] = clientConfig
+	}
+
+	return nil
+}
