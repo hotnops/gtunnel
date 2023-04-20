31f9a36048e19711b968bd52a1194574c96076c6
hotnops <hotnops@protonmail.com>
2023-04-20 08:31:43 -0400

    Updating to protoc 22.3 and including cgo file for windows builds. Silencing the docker cp output that spams the terminal. (#13)

---
+++ gclient/cgo_windows.go
@@ -0,0 +1,8 @@
+package main
+
+import "C"
+
+//export ExportMain
+func ExportMain() {
+	main()
+}
--- grpc/build_protoc.sh
+++ grpc/build_protoc.sh
@@ -1,2 +1,2 @@
-protoc -I client client/client.proto --go_out=plugins=grpc:client/ --go_opt=paths=source_relative
-protoc -I admin admin/admin.proto --go_out=plugins=grpc:admin/ --go_opt=paths=source_relative
+protoc -I=client --go_out=. --go-grpc_out=. client/client.proto
+protoc -I=admin --go_out=. --go-grpc_out=. admin/admin.proto 
