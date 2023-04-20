c89dc9b972748170b5086231a62bf19235a64ac5
hotnops <hotnops@protonmail.com>
2023-04-20 10:46:45 -0400

    Fixing output directory for protoc

--- grpc/build_protoc.sh
+++ grpc/build_protoc.sh
@@ -1,2 +1,2 @@
-protoc -I=client --go_out=. --go-grpc_out=. client/client.proto
-protoc -I=admin --go_out=. --go-grpc_out=. admin/admin.proto 
+protoc -I=client --go_out=client/ --go-grpc_out=client/ client/client.proto
+protoc -I=admin --go_out=admin/ --go-grpc_out=admin/ admin/admin.proto 
